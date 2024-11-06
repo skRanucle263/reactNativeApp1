dd4c95ebed7b50c2af33f9ab1368cbf0ed24bdaa
Soham <dan@ranucle.com>
2024-11-06 15:41:29 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,10 +4,8 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
